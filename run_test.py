9d41178e9a60d2a6125bf3f1147adcf1748b3e36
Abdelmageed <83831812+abdelmageed95@users.noreply.github.com>
2022-04-15 02:26:24 +0200

    edit images

--- run_test.py
+++ run_test.py
@@ -1,5 +1,5 @@
 from yolov5.detect import main ,parse_opt
 
 if __name__ == "__main__":
-    opt = parse_opt(weights_path =r"C:\Users\alaa\Desktop\Crown-starfish-detection\weights\aug_best.pt",test_images_path=r"C:\Users\alaa\Downloads\Raja Ampat Crown of Thorns Removal Vinegar Injection.mp4",data_yaml="starfish_data/data.yaml")
+    opt = parse_opt(weights_path =r"weights\aug_best.pt",test_images_path=r"C:\Users\alaa\Downloads\Raja Ampat Crown of Thorns Removal Vinegar Injection.mp4",data_yaml="starfish_data/data.yaml")
     main(opt)
